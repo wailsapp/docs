f4073b9eece8e8caba926a496b28af5f3bd39478
Lea Anthony <lea.anthony@gmail.com>
2019-04-30 20:28:15 +1000

    tidy up

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,8 +1,4 @@
 
-// const glob = require('glob');
-
-// let tutorialfiles = glob.sync('docs/**/*.md').map(f => '/' + f); 
-
 module.exports = {
   title: "Wails",
   description:
@@ -12,65 +8,8 @@ module.exports = {
   },
   themeConfig: {
     sidebar: {
-      // "/tutorials/": tutorialfiles, 
-      // [
-      // ["/home", "← Back"]
-      // {
-      //   title: "Basics",
-      //   collapsable: true,
-      //   children: ["01 - Basic", "02 - Basic Custom", "03 - Custom HTML"]
-      // },
-      // {
-      //   title: "Binding",
-      //   collapsable: true,
-      //   children: ["04 - Binding Functions", "05 - Binding Structs"]
-      // },
-      // {
-      //   title: "Events",
-      //   collapsable: true,
-      //   children: [
-      //     "06 - Events 1",
-      //     "07 - Events 2",
-      //     "08 - Events 3",
-      //     "09 - Events 4"
-      //   ]
-      // },
-      // {
-      //   title: "Errors, Logging, Assets",
-      //   collapsable: true,
-      //   children: [
-      //     "10 - Error Handling",
-      //     "11 - Logging",
-      //     "12 - Custom Logging",
-      //     "13 - Assets"
-      //   ]
-      // },
-      // {
-      //   title: "Dialogs",
-      //   collapsable: true,
-      //   children: [
-      //     "14 - Dialog - Select File",
-      //     "15 - Dialog - Select Directory",
-      //     "16 - Dialog - Save File"
-      //   ]
-      // },
-      // {
-      //   title: "Window",
-      //   collapsable: true,
-      //   children: ["17 - Window - Colour", "18 - Window - Control"]
-      // }
-      // ],
       "/": ["home", "quick_start", "/reference/", "/tutorials/", "project_status"]
-      // "/": ["home", "quick_start", "cli", "guide", "tutorials"]
     },
-    //       {
-    //   title: 'Tutorials',
-    //     collapsable: true,
-    //       children: [
-    //         '/tutorials/'
-    //       ]
-    // },
-    //     ],
     repo: "wailsapp/wails",
     // repoLabel: 'Contribute!',
     // if your docs are in a different repo from your main project:
@@ -84,8 +23,6 @@ module.exports = {
     // custom text for edit link. Defaults to "Edit this page"
     // editLinkText: 'Help us improve this page!',
     nav: [{ text: "Home", link: "/" }, { text: "Twitter", link: "https://twitter.com/wailsapp" }, { text: "Slack", link: "https://wails.slack.com" }],
-    plugins: [
-      ['minimal-analytics', { ga: 'UA-139310949-1' }]
-    ]
+    ga: 'UA-139310949-1'
   }
 };
